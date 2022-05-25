835a38f3486a281258a9600b04932d04d799b01f
Raon98 <cheol1998@kakao.com>
2022-05-25 11:20:33 +0900

    computedEx

---
+++ src/views/computedEx.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p>{{ticketList}}</p>
+    <p>{{increse}}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "computedEx",
+  data(){
+    return{
+      score: 1
+    };
+  },
+  computed: {
+    increse(){
+      // eslint-disable-next-line no-undef
+      return this.score +1
+      },
+    ticketList(){
+      return this.$store.getters["ticket/ticket"];
+      //ticket.js라는 store에 접근해 ticket state를 가져온 후 템플릿에 보입니다.
+    }
+    }
+  }
+
+</script>
+
+<style scoped>
+
+</style>
